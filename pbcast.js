6e013f6c8334c355a8dfda73b2b3794b4925ab60
Gal Ben-Haim <gbenhaim@augury.com>
2016-02-13 00:35:37 +0200

    support forum and blogs

--- pbcast.js
+++ pbcast.js
@@ -42,7 +42,37 @@ function addLinksToNews() {
       $( "#cast-bar-"+numDlBar ).append( "<a class='pb-button small' href='"+value+"' target='_blank'>"+"<img src='" +url+"' width=\"20\" style=\"vertical-align: middle; margin-bottom: 0.15em;\"/>"+result+"</a>" );
 		});
 	});
+};
 
+function addLinksToForum() {
+	var numDlBar = 0;
+	var videoNum = "";
+	var firstRun = true;
+	$( ".details" ).each(function( index ) {
+		$(this).after( "<div class='button-bar switched' id='cast-bar-"+numDlBar+"'></div>" );
+		numDlBar = numDlBar + 1;
+	});
+	numDlBar = 0;
+	var extract = $( "script:contains('source')" ).each(function( index ) {
+		var indexscope = $( this ).text();
+		var resolvelinks = indexscope.match(/\bhttp\S*mp4\b/gm)
+		$.each(resolvelinks, function( index, value ) {
+			n = value.lastIndexOf('/');
+			if ( firstRun == true ) {
+				videoNum = value.substring(n + 1);
+				firstRun = false;
+			}
+			if (value.substring(n + 1) != videoNum){
+				numDlBar = numDlBar + 1;
+				videoNum = value.substring(n + 1);
+			}
+			var patt = /vm|vf|v1280|v1920/g;
+			var result = patt.exec(value).toString().replace("vm", "240p").replace("vf","480p").replace("v1280","720p").replace("v1920","1080p");
+			var value = "https://vidcast.dabble.me/index.html?video_link=" + encodeURIComponent(value) + "&submit=Go";
+      var url = chrome.extension.getURL('ic_cast2_black_24dp.png');
+      $( "#cast-bar-"+numDlBar ).append( "<a class='pb-button small' href='"+value+"' target='_blank'>"+"<img src='" +url+"' style=\"vertical-align: middle; margin-bottom: 0.15em; display: inline-block; width: 20px !important;\"/>"+result+"</a>" );
+		});
+	});
 };
 
 if (document.title.indexOf("Pinkbike") != -1) {
@@ -54,8 +84,14 @@ if (document.title.indexOf("Pinkbike") != -1) {
 		if(window.location.href.indexOf("/news/") > -1) {
 		   addLinksToNews();
 		}
-		if(window.location.href.indexOf("/video/") > -1) {
+		else if(window.location.href.indexOf("/video/") > -1) {
 		   addLinksToVideo();
+		}
+    else if(window.location.href.indexOf("/forum/") > -1) {
+		   addLinksToForum();
+		}
+    else if(window.location.href.indexOf("/blog/") > -1) {
+		   addLinksToForum();
 		}
 	});
 };
